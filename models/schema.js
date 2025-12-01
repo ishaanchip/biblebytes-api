f64c1e7fd217ee6b42b750e470c83200ee436efb
Ishaan Chip <ishaanchiplunkar@gmail.com>
2025-12-01 01:21:19 -0500

    commentary route update

--- models/schema.js
+++ models/schema.js
@@ -10,9 +10,39 @@ const clientAccountSchema = new Schema({
     create_date:{type:Date, default: Date.now},
 })
 
+//commentary schema
+const commentaryTemplateSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      type: {
+        type: String,
+        default: "Series"
+      },
+      commentaries: {
+        type: [Object],
+        default: []
+      },
+      commentary_description:{
+        type:String,
+      },
+      img_url:{
+        type:String,
+        default:"",
+      },
+      create_date: {
+        type: Date,
+        default: Date.now
+      }
+})
+
 const clientAccounts = mongoose.model('client_accounts', clientAccountSchema, 'client_accounts');
 
+const commentaryTemplate = mongoose.model('commentaries', commentaryTemplateSchema, 'commentaries');
 
-const mySchemas = {'clientAccounts':clientAccounts}
+
+const mySchemas = {'clientAccounts':clientAccounts, 'commentaryTemplate':commentaryTemplate}
 
 module.exports = mySchemas;
--- routes/bibleRouter.js
+++ routes/bibleRouter.js
@@ -6,7 +6,7 @@ const schemas = require("../models/schema");
 //GENERAL ROUTES
 
 
-//HOME ROUTES
+//BIBLE ROUTES
 
     //getting full chapter of user-inputted book & chapter
     router.post('/get-full-passage', async(req, res) =>{
@@ -19,8 +19,7 @@ const schemas = require("../models/schema");
 
             //2. return the data
             if (result) {
-                console.log(result.data)
-                console.log("bible chapter successfully fetched!");
+                //console.log("bible chapter successfully fetched!");
                 return res.status(200).json({bibleData: result.data });
               } else {
                 console.error("Failed to fetch bible data.");
@@ -35,6 +34,93 @@ const schemas = require("../models/schema");
     })
 
 
+//COMMENTARY ROUTES
+
+    //GET [retrieve every document]
+    router.get('/get-commentary-catalog', async(req, res) =>{
+        try{
+            //0. base variables
+            const commentaryQuery = schemas.commentaryTemplate
+            
+            //1. getting documents
+            const result = await commentaryQuery.find({})
+
+            //2. sending documents to frontend
+            res.status(200).json({result})
+        }
+        catch(err){
+            console.log(`there was a backend error getting commentary catalog: ${err}`)
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    })
+
+    //POST [creating commentary collection]
+    router.post('/create-commentary-template', async(req, res) =>{
+        try{
+            //0. base variables
+            const commentaryQuery = schemas.commentaryTemplate
+            const {commentaryToInsert} = req.body;
+
+            //1. creating commentary in mongodb
+            const newCommentaryTemplate =  new commentaryQuery(commentaryToInsert)
+            const result = await newCommentaryTemplate.save()
+            console.log('template made ! ! !')
+
+        }
+        catch(err){
+            console.log(`there was a backend error posting commentary template: ${err}`)
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    })
+
+    //PUT [adding chapter to exisiting commentary series]
+    router.put('/add-commentary-chapter', async(req, res) =>{
+        try{
+            //0. base variables
+            const commentaryQuery = schemas.commentaryTemplate
+            const {chapterToInsert, commentaryName} = req.body
+
+            //1. putting chapter in corresponding commentary template
+            const result = await commentaryQuery.updateOne(
+                {name:commentaryName},
+                {$push: {commentaries: chapterToInsert}}
+            )
+            console.log("Modified Docments: " + result.modifiedCount)
+            if (result){
+                return res.status(201).json({ message: "commentary updated successfully!"});
+            }
+
+        }
+        catch(err){
+            console.log(`there was a backend error posting commentary chapters: ${err}`)
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    })
+
+
+    //POST [retrieving chapter of commentary series]
+    router.put('/retrieve-commentary-chapter', async(req, res) =>{
+        try{
+            //0. base variables
+            const commentaryQuery = schemas.commentaryTemplate
+            const {commentaryName} = req.body
+
+            //1. putting chapter in corresponding commentary template
+            const result = await commentaryQuery.findOne(
+                {name:commentaryName},
+                {$push: {commentaries: chapterToInsert}}
+            )
+            console.log("Modified Docments: " + result.modifiedCount)
+            if (result){
+                return res.status(201).json({ message: "commentary updated successfully!"});
+            }
+
+        }
+        catch(err){
+            console.log(`there was a backend error posting commentary chapters: ${err}`)
+            return res.status(500).json({ error: "Internal server error" });
+        }
+    })
 
 
 module.exports = router;
